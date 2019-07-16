23abedb6ca1ca760c3472c64ee37622d5fb0b9a5
Roger Bell_West <roger@firedrake.org>
2019-07-16 16:18:53 +0100

    Tidy output

--- bankholidays.pl
+++ bankholidays.pl
@@ -19,7 +19,9 @@ sub generate {
   my $cal=Data::ICal->new;
   if ($calstr) {
     $calstr =~ s/’/'/g;
-    $calstr =~ s/(SUMMARY:[\w ]*)/$1 Bank Holiday/g;
+    unless ($calstr =~ /bank holiday/i) {
+      $calstr =~ s/(SUMMARY:[\w ]*)/$1 Bank Holiday/g;
+    }
     $cal=Data::ICal->new(data => $calstr);
   }
   return $cal;
